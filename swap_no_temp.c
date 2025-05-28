f4b8555c53f270aa06fabcecbe59008663a39d4b
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-28 15:26:55 +0200

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int a , b;
-
+    int a, b;
+    scanf("%d %d", &a, &b);
     
-    scanf("%d %d", &a, &b); // scanf permet de recuperer et de stocker l'entrée de l'utilisateur 
-//commencons maintenant l'inversion avec une simple règle mathématique
-
- a = a + b;
- b = a - b;
- a = a - b;
-
-
- printf("Apres echange : a = %d, b = %d", a, b);
- return 0;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    printf("Après échange: a = %d, b = %d\n", a, b);
+    return 0;
 }
